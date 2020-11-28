fb3e3c6b0e6e79eca51344181769a98e77633406
Attila Serfozo <s.attila0417@gmail.com>
2020-11-28 20:35:58 +0100

    uploading the Task structures

---
+++ Task_2_covid_cases/codes/analysis_covid_AS.R
@@ -0,0 +1,11 @@
+########################
+##  Attila Serfozo    ##
+## Assignment for DA2 ##
+##  and for Coding    ##
+##                    ##
+##   NO. 3            ##
+## Data Analysis      ##
+########################
+
+
+
